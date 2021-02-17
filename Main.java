1862648ec038b45c95b0db94bac22b46881d51b6
pmaniski <79202882+pmaniski@users.noreply.github.com>
2021-02-17 13:34:42 +0000

    Update Main.java

--- Main.java
+++ Main.java
@@ -11,9 +11,10 @@ public class Main {
     public static void main(String args[]) {
      
         Model model = new Model();               // Create shared database
-        Controller1 c1 = new Controller1(model); // Create first controller
-        Controller2 c2 = new Controller2(model); // Create second controller
-        
+        Controller1 c1 = new Controller1(model, "frameTitle0"); // Create first controller
+        Controller2 c2 = new Controller2(model, "frameTitle1"); // Create second controller
+        Controller2 c3 = new Controller2(model, "frameTitle2");
+        Controller2 c4 = new Controller2(model, "frameTitle3");
     } // main
   
 } // Main
