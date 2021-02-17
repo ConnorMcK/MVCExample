1895352245e4dbcac167b9c2660888f869f5a3a2
deadWhole <56160482+deadWhole@users.noreply.github.com>
2021-02-17 12:48:46 +0000

    Changed the constructor of Controller1 & Controller2 to include a String parameter used for setting the title of the frames

--- Controller2.java
+++ Controller2.java
@@ -1,38 +1,36 @@
-package mvcexample;
-
 // Controller 2: displays views 3 and 4
 //               (displaying A and B components of the model)
 // Offers two button: clear views and refresh views - see below.
 // The refresh views button is required as views 3 and 4 do not yet
 // subscribe properly to the model and are not notified when it changes.
 
-import java.awt.*;
-import java.awt.event.*;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Controller2 extends JFrame
-                         implements ActionListener {
-  
+public class Controller2 extends JFrame implements ActionListener {
+
     private Model model;
     private View3 view3;
     private View4 view4;
     private JButton clearViews;   // For direct message to views
     private JButton refreshViews; // To prompt them to refresh their contents from the model
- 
+
     // Constructor
-    public Controller2(Model model) {
-    
+    public Controller2(Model model, String frameTitle) {
+
         // Record reference to the model
         this.model = model;
-        
+
         // Configure the window
-        setTitle("Controller2");
-        setLocation(40,200);
-        setSize(350,150);
+        setTitle(frameTitle);
+        setLocation(40, 200);
+        setSize(350, 150);
         setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
         Container window = getContentPane();
         window.setLayout(new FlowLayout());     // The default is that JFrame uses BorderLayout
-        
+
         // Set up input GUI
         clearViews = new JButton("Clear views");
         window.add(clearViews);
@@ -45,15 +43,15 @@ public class Controller2 extends JFrame
         window.add(view3);
         view4 = new View4(this, model);
         window.add(view4);
-        
+
         // Display the frame
         setVisible(true);
-        
+
     } // constructor
-  
+
     // Button click handling:
     public void actionPerformed(ActionEvent e) {
-      
+
         if (e.getSource() == clearViews) {
             view3.clear();
             view4.clear();
@@ -62,9 +60,9 @@ public class Controller2 extends JFrame
             view3.update();
             view4.update();
         }
-        
+
     } // actionPerformed
-    
+
 } // class Controller2
 
 
